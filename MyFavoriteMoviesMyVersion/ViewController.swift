edd6d4e51485f9675310e8b0924aa0d40ef55a54
iOSMitchSalcido <iosmitchsalcido@gmail.com>
2017-04-07 06:28:38 -0700

    Initial Commit

---
+++ MyFavoriteMoviesMyVersion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFavoriteMoviesMyVersion
+//
+//  Created by Online Training on 4/7/17.
+//  Copyright © 2017 Mitch Salcido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
